834e3577568fbdf93f5b78d031cb668009a2f923
lex <yobdc.101@gmail.com>
2020-09-08 16:24:53 +0800

    维表缓存

--- src/main/java/com/yobdcdoll/table/KafkaTableTest.java
+++ src/main/java/com/yobdcdoll/table/KafkaTableTest.java
@@ -11,6 +11,10 @@ import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
+/**
+ * kafka表为order事件，users为mysql维度表
+ * users表缓存1min
+ */
 public class KafkaTableTest {
     public static void main(String[] args) throws Exception {
         ParameterTool parameterTool = ParameterTool.fromPropertiesFile(
@@ -33,6 +37,9 @@ public class KafkaTableTest {
                 ",userId bigint" +
                 ",itemId bigint" +
                 ",createTime bigint" +
+                ",proc_time as proctime()"+
+//                ",eventTime as to_timestamp(from_unixtime(createTime/1000, 'yyyy-MM-dd HH:m:ss'))"+
+//                ",watermark for eventTime as eventTime - interval '5' second"+
                 ") with (" +
                 "'connector' = 'kafka'" +
                 ",'topic' = '" + topic + "'" +
@@ -47,15 +54,15 @@ public class KafkaTableTest {
                 ",name string" +
                 ",age int" +
                 ",city string" +
-                ",primary key (userId) not enforced" +
+//                ",primary key (userId) not enforced" +
                 ") with (" +
                 "'connector' = 'jdbc'" +
-                ",'url' = 'jdbc:mysql://192.168.34.1:3306/dbmeta'" +
+                ",'url' = 'jdbc:mysql://localhost:3306/dbmeta'" +
                 ",'table-name' = 'users'" +
                 ",'username'='root'" +
                 ",'password'='root'" +
-                ",'lookup.cache.max-rows'='80'" +
-                ",'lookup.cache.ttl'='10s'" +
+                ",'lookup.cache.max-rows'='1000'" +
+                ",'lookup.cache.ttl'='1min'" +
                 ")");
 
         Table orders = env.sqlQuery("select orders.orderId" +
@@ -65,7 +72,9 @@ public class KafkaTableTest {
                 ", users.age" +
                 ", users.city" +
                 " from orders" +
-                " left join users on users.userId = orders.userId");
+                " left join users" +
+                " for system_time as of orders.proc_time" +
+                " on users.userId = orders.userId");
         DataStream<Tuple2<Boolean, Row>> outStream = env.toRetractStream(orders, Row.class);
         outStream.print();
         streamEnv.execute("Print kafka table");
